0f2a1fa8e4015714d4cdc33618f72c481c46acbc
Emily Plummer <emplums@github.com>
2020-01-31 13:36:42 -0800

    clean up decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,17 +3,7 @@ import { addDecorator, addParameters } from '@storybook/react'
 import {BaseStyles, Link, Heading, Flex} from '../src'
 import { withKnobs} from '@storybook/addon-knobs'
 
-// import {Code, InlineCode, Table, Image, Paragraph, HorizontalRule, Blockquote, Heading, List, DescriptionList} from '@primer/gatsby-theme-doctocat'
-const {H1, H2, H3, H4, H5, H6} = Heading
-const components = {
-  a: Link,
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  h4: H4,
-  h5: H5,
-  h6: H6,
-}
+
 addDecorator(storyFn => {
   return (
     <BaseStyles>
@@ -25,5 +15,3 @@ addDecorator(storyFn => {
 });
 
 addDecorator(withKnobs)
-
-addParameters({docs: {components}});
