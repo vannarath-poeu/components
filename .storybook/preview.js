41a4af8385f6e0c9aab0962d7490aca7ee23277c
Emily Plummer <emplums@github.com>
2020-01-31 16:24:06 -0800

    fix dialog story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {addDecorator, addParameters} from '@storybook/react'
-import {BaseStyles, Link, Heading, Flex} from '../src'
+import {addDecorator} from '@storybook/react'
+import {BaseStyles, Box} from '../src'
 import {withKnobs} from '@storybook/addon-knobs'
 
 addDecorator(storyFn => {
   return (
     <BaseStyles>
-      <Flex justifyContent="center" p={4}>
+      <Box p={4}>
         {storyFn()}
-      </Flex>
+      </Box>
     </BaseStyles>
   )
 });
